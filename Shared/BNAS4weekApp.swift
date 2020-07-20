73e3cae84d297f0c1eec0f5d67ac363da678a92c
Ola Göransson <ola.goransson@me.com>
2020-07-20 15:46:15 +0200

    Initial Commit

---
+++ Shared/BNAS4weekApp.swift
@@ -0,0 +1,17 @@
+//
+//  BNAS4weekApp.swift
+//  Shared
+//
+//  Created by Ola Göransson on 2020-07-20.
+//
+
+import SwiftUI
+
+@main
+struct BNAS4weekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
